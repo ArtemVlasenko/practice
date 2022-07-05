c30214f3e7c8d5c864caac9b5b1fbb28334c9248
Al Bal <differencialx@gmail.com>
2022-07-05 17:19:45 +0300

    TMP

--- dz_11_243.rb
+++ dz_11_243.rb
@@ -4,15 +4,14 @@
 # включив в первый из них элементы заданного массива с четными индексами,
 # а во второй — с нечетными.
 
-array = [100, 2, 2, 3, 4, 3, 4, 10, 11, 15, 20, 25, 30, 35, 45, 55, 55, 55, 55, 45]
+array = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
 
 def two_arrays_even_and_odd_elements(array)
-
-  # array.group_by.with_index { |x, i| x % 2  }.values
-  array.partition{ |i| i[0].zero? }
-
+  array.group_by.with_index do |x, i|
+    i.even?
+  end.values
 end
-print two_arrays_even_and_odd_elements(array)
+puts two_arrays_even_and_odd_elements(array).inspect
 
 
 # group_by - возвращаемый объект будет хешем
--- dz_12_23.rb
+++ dz_12_23.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
-# def populate_a_two_array
+def compose_array
+  array = []
+  row = 0
+  col = 0
 
-
-# end
+  while row < 7
+    array[row] = []
+    while col < 7
+    end
+  end
+end
 
 size = 7
 
-  a = Array.new(size, '*').fill('=', 1..-2).join(' ') #=> "* = = = = = *"
-  b = Array.new(size, '=').fill('*', 1..-2).join(' ')
-  c = Array.new(size, '=').fill('*', 2..-3).join(' ')
-  f = Array.new(size, '=').fill('*', 3..-4).join(' ')
+a = Array.new(size, '*').fill('=', 1..-2).join(' ') #=> "* = = = = = *"
+b = Array.new(size, '=').fill('*', 1..-2).join(' ')
+c = Array.new(size, '=').fill('*', 2..-3).join(' ')
+f = Array.new(size, '=').fill('*', 3..-4).join(' ')
 pattern = [a, b, c, f]
+
 puts pattern.take(size)
