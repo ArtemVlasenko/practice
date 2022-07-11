afc6967893dc2df45c1d0bfe0c08b6c3f1efda1b
Al Bal <differencialx@gmail.com>
2022-07-11 11:10:43 +0300

    TMP

--- dz_13_6.rb
+++ dz_13_6.rb
@@ -4,11 +4,19 @@
 # Все значения выражены в км/ч.
 # Напечатать названия моделей, у которых максимальная скорость превышает 180 км/ч.
 
-hash = {"vaz" => 100, "mazda" => 110, "suzuki" => 150, "audi" => 160,
-"fiat" => 170, "kia" => 180, "hyundai" => 190, "bmw" => 200}
+hash = {
+  "vaz" => 100,
+  "mazda" => 110,
+  "suzuki" => 150,
+  "audi" => 160,
+  "fiat" => 170,
+  "kia" => 180,
+  "hyundai" => 190,
+  "bmw" => 200
+}
 
 def maximum_speed(hash)
-  hash.select { |el, number| number >= 180 }
+  hash.select { |el, number| number > 180 }
 end
 
 puts "Task -> input: #{hash}  output: #{maximum_speed(hash)}"
